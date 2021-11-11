d546ef5af0cb443fbb5886db4fedbb655d0394ba
Liu Li <nox60@hotmail.com>
2021-11-12 00:06:13 +0800

    Mulit-threads datax

--- src/main/java/com/dataxservice/dataxservice/controllers/SimpleController.java
+++ src/main/java/com/dataxservice/dataxservice/controllers/SimpleController.java
@@ -5,18 +5,34 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.alibaba.datax.core.Engine;
 
+
+
 @RestController
 public class SimpleController {
 
     @RequestMapping(value = "/t", method = RequestMethod.GET)
     public String hello() {
+        System.out.println("--->>");
+        System.setProperty("datax.home", "D:\\dev\\workspaces\\DataX\\target\\datax\\datax");
+        String[] datxArgs = {"-job", "D:\\dev\\workspaces\\datax_java_without_python\\src\\main\\resources\\stream2stream.json", "-mode", "standalone", "-jobid", "-1"};
+//
+        try {
+            Engine.entry(datxArgs);   //从这里启动
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
         return "Hello world";
     }
 
     public static void main(String args[]) {
         System.out.println("--->>");
         System.setProperty("datax.home", "D:\\dev\\workspaces\\DataX\\target\\datax\\datax");
-//        String[] datxArgs = {"-job", "E:\\Document\\intelliidea\\dataxText\\src\\main\\resources\\stream2stream.json", "-mode", "standalone", "-jobid", "-1"};
+        String[] datxArgs = {"-job", "D:\\dev\\workspaces\\datax_java_without_python\\src\\main\\resources\\stream2stream.json", "-mode", "standalone", "-jobid", "-1"};
 //
+        try {
+            Engine.entry(datxArgs);   //从这里启动
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
     }
 }
