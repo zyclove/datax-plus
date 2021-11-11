418934c36e60d4109d940496a11f10740907d32e
Liu Li <nox60@hotmail.com>
2021-11-11 23:55:14 +0800

    Mulit-threads datax

--- src/main/java/com/dataxservice/dataxservice/controllers/SimpleController.java
+++ src/main/java/com/dataxservice/dataxservice/controllers/SimpleController.java
@@ -3,6 +3,7 @@ package com.dataxservice.dataxservice.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import com.alibaba.datax.core.Engine;
 
 @RestController
 public class SimpleController {
@@ -12,4 +13,10 @@ public class SimpleController {
         return "Hello world";
     }
 
+    public static void main(String args[]) {
+        System.out.println("--->>");
+        System.setProperty("datax.home", "D:\\dev\\workspaces\\DataX\\target\\datax\\datax");
+//        String[] datxArgs = {"-job", "E:\\Document\\intelliidea\\dataxText\\src\\main\\resources\\stream2stream.json", "-mode", "standalone", "-jobid", "-1"};
+//
+    }
 }
