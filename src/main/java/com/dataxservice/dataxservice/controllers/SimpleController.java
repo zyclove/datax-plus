619b94325e9683959d4ae6cb408b360b5f1ed681
LiuLi <nox60@hotmail.com>
2021-11-12 14:48:19 +0800

    Test mysql job

--- src/main/java/com/dataxservice/dataxservice/controllers/SimpleController.java
+++ src/main/java/com/dataxservice/dataxservice/controllers/SimpleController.java
@@ -24,6 +24,20 @@ public class SimpleController {
         return "Hello world";
     }
 
+    @RequestMapping(value = "/mysql", method = RequestMethod.GET)
+    public String mysql() {
+        System.out.println("--->>");
+        System.setProperty("datax.home", "D:\\dev\\workspaces\\DataX\\target\\datax\\datax");
+        String[] datxArgs = {"-job", "D:\\dev\\workspaces\\datax_java_without_python\\src\\main\\resources\\stream2stream.json", "-mode", "standalone", "-jobid", "-1"};
+//
+        try {
+            Engine.entry(datxArgs);   //从这里启动
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        return "Hello world";
+    }
+
     public static void main(String args[]) {
         System.out.println("--->>");
         System.setProperty("datax.home", "D:\\dev\\workspaces\\DataX\\target\\datax\\datax");
