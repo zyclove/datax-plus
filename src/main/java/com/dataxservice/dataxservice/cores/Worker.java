8f24bdd4d22c136d9c8f97e3be126241cceeedf8
LiuLi <nox60@hotmail.com>
2021-11-16 16:29:24 +0800

    Test mysql job

--- src/main/java/com/dataxservice/dataxservice/cores/Worker.java
+++ src/main/java/com/dataxservice/dataxservice/cores/Worker.java
@@ -3,10 +3,14 @@ package com.dataxservice.dataxservice.cores;
 import com.alibaba.datax.core.Engine;
 
 public class Worker implements Runnable {
+    
+    private String jsonFileName = "";
+
+    
 
     @Override
     public void run() {
-        System.setProperty("datax.home", "D:\\dev\\workspaces\\DataX\\target\\datax\\datax");
+//        System.setProperty("datax.home", "D:\\dev\\workspaces\\DataX\\target\\datax\\datax");
         String[] datxArgs = {"-job", "D:\\dev\\workspaces\\datax_java_without_python\\src\\main\\resources\\mysql2mysql.json", "-mode", "standalone", "-jobid", "998877"};
         try {
             Engine.entry(datxArgs);   //从这里启动
