8d94f084ac1ec2654e3bb138ab833bb31da2b965
Liu Li <nox60@hotmail.com>
2021-11-16 21:12:28 +0800

    Add Jenkinsfile to Repo

--- src/main/resources/log_processor_filebeats/run.sh
+++ src/main/resources/log_processor_filebeats/run.sh
@@ -1,8 +1,5 @@
 docker run -d \
   --name=filebeat \
   --user=root \
-  --volume="$(pwd)/filebeat.yml:/usr/share/filebeat/filebeat.yml:ro" \
-  docker.elastic.co/beats/filebeat:7.15.2 filebeat -e -strict.perms=false \
-  -E output.elasticsearch.hosts=["elasticsearch:9200"]
-
-
+  --volume="$(pwd)/filebeats.yml:/usr/share/filebeat/filebeat.yml:ro" \
+  docker.elastic.co/beats/filebeat:7.15.2 filebeat
