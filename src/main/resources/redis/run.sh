3284ebb461e21df99ad6abfdaf2f30f301acda0d
Liu Li <nox60@hotmail.com>
2021-11-17 00:21:39 +0800

    Add Jenkinsfile to Repo

--- src/main/resources/redis/run.sh
+++ src/main/resources/redis/run.sh
@@ -1,4 +1,4 @@
 docker run --name simple-redis -d \
 -p 6379:6379 \
 --volume="$(pwd)/redis.conf:/usr/local/etc/redis/redis.conf:ro" \
-redis:6.2.6-alpine /usr/local/etc/redis/redis.conf
+redis:5.0.0-alpine /usr/local/etc/redis/redis.conf
