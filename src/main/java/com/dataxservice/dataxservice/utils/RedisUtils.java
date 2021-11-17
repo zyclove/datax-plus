cf0c2f45c9f996678caaaae74b89e9a490f1d7b1
LiuLi <nox60@hotmail.com>
2021-11-17 15:15:31 +0800

    Test mysql job

--- src/main/java/com/dataxservice/dataxservice/utils/RedisUtils.java
+++ src/main/java/com/dataxservice/dataxservice/utils/RedisUtils.java
@@ -29,25 +29,15 @@ public final class RedisUtils {
      */
 
     public boolean expire(String key, long time) {
-
         try {
-
             if (time > 0) {
-
                 redisTemplate.expire(key, time, TimeUnit.SECONDS);
-
             }
-
             return true;
-
         } catch (Exception e) {
-
             e.printStackTrace();
-
             return false;
-
         }
-
     }
 
     /**
