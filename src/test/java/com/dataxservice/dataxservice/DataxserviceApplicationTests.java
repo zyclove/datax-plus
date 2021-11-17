ad5db751f5ec83b7d78d98998c2c0d1d2b280e8a
LiuLi <nox60@hotmail.com>
2021-11-17 15:24:24 +0800

    Implement log consumer

--- src/test/java/com/dataxservice/dataxservice/DataxserviceApplicationTests.java
+++ src/test/java/com/dataxservice/dataxservice/DataxserviceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DataxserviceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
