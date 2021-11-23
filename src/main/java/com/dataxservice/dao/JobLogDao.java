8a730aa17fd23a1a2fab779da08490f0b010945c
LiuLi <nox60@hotmail.com>
2021-11-23 14:17:35 +0800

    Add job log into database

--- src/main/java/com/dataxservice/dao/JobLogDao.java
+++ src/main/java/com/dataxservice/dao/JobLogDao.java
@@ -1,8 +1,12 @@
 package com.dataxservice.dao;
 
+import com.dataxservice.models.JobLog;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface JobLogDao {
     int testCount();
+
+    int addLog(@Param("jobLog") JobLog jobLog);
 }
