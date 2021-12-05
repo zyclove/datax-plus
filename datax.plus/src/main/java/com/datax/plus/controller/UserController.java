7b43d09b296668ed7890c7535a5c6f35d737f33c
Liu Li <nox60@hotmail.com>
2021-12-05 11:45:17 +0800

    Update package settings

--- datax.plus/src/main/java/com/datax/plus/controller/UserController.java
+++ datax.plus/src/main/java/com/datax/plus/controller/UserController.java
@@ -1,17 +1,30 @@
 package com.datax.plus.controller;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.datax.plus.model.HttpRequestResult;
 import com.datax.plus.model.User;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
     @RequestMapping(value = "/checkLogin", method = RequestMethod.POST)
-    public String checkLogin(@RequestBody User user) {
-        return String.valueOf(1);
+    public @ResponseBody
+    HttpRequestResult checkLogin(@RequestBody User user) {
+        HttpRequestResult req = new HttpRequestResult();
+        req.setCode(200);
+        req.setMsg("");
+
+        // https://www.jianshu.com/p/e88d3f8151db
+
+        // JWT create 方法，
+        String token= JWT.create().withAudience("user.getId()").sign(Algorithm.HMAC256(user.getPassword()));
+
+//        String jwtToken = Jwts.builder().setSubject(reqPerson.getUsername()).claim("roles", "member").setIssuedAt(new Date())
+//                .signWith(SignatureAlgorithm.HS256, "secretkey").compact();
+
+        return req;
     }
 }
---
+++ datax.plus/src/main/java/com/datax/plus/model/HttpRequestResult.java
@@ -0,0 +1,40 @@
+package com.datax.plus.model;
+
+public class HttpRequestResult {
+    private int code;
+    private String msg;
+    private String token;
+    private Object data;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
