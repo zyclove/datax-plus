4fde1546c355be08717856be682d578945fa7741
LiuLi <nox60@hotmail.com>
2021-12-07 17:44:11 +0800

    Add job related interfaces

---
+++ datax.plus/src/main/java/com/datax/plus/controller/JobController.java
@@ -0,0 +1,54 @@
+package com.datax.plus.controller;
+
+import com.datax.plus.model.HttpRequestResult;
+import com.datax.plus.model.User;
+import com.datax.plus.util.JwtUtil;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/job")
+public class JobController {
+
+
+    @RequestMapping(value = "/userInfo", method = RequestMethod.GET)
+    public @ResponseBody
+    HttpRequestResult userInfo() {
+        HttpRequestResult req = new HttpRequestResult();
+        req.setCode(20000);
+        req.setMsg("");
+        User repUserObj = new User();
+        repUserObj.setCode(100);
+        List<String> roles = new ArrayList<String>();
+        roles.add("admin");
+        repUserObj.setRoles(roles);
+        req.setData(repUserObj);
+        return req;
+    }
+
+    @RequestMapping(value = "/checkLogin", method = RequestMethod.POST)
+    public @ResponseBody
+    HttpRequestResult checkLogin(@RequestBody User user) {
+        HttpRequestResult req = new HttpRequestResult();
+        req.setCode(20000);
+        req.setMsg("");
+
+        // https://www.jianshu.com/p/e88d3f8151db
+
+        // JWT create 方法，
+   //     String token= JWT.create().withAudience("user.getId()").sign(Algorithm.HMAC256(user.getPassword()));
+//        String jwtToken = Jwts.builder().setSubject(reqPerson.getUsername()).claim("roles", "member").setIssuedAt(new Date())
+//                .signWith(SignatureAlgorithm.HS256, "secretkey").compact();
+        Map<String, String > payload = new HashMap<String, String>();
+        String token = JwtUtil.signJwt(user, payload);
+        req.setToken(token);
+        User repUserObj = new User();
+        repUserObj.setCode(100);
+        req.setData(repUserObj);
+        return req;
+    }
+}
