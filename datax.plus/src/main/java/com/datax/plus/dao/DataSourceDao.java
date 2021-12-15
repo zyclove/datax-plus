15318e81d71cb762c0a33acecf4835db4e65ebbd
Liu Li <nox60@hotmail.com>
2021-12-15 23:59:18 +0800

    Update datasource mapper

--- datax.plus/src/main/java/com/datax/plus/dao/DataSourceDao.java
+++ datax.plus/src/main/java/com/datax/plus/dao/DataSourceDao.java
@@ -1,5 +1,14 @@
 package com.datax.plus.dao;
 
+import com.datax.plus.model.DataJob;
+import com.datax.plus.model.DataSource;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
 public interface DataSourceDao {
+    int addDataSource(@Param("dataSource") DataSource dataSource);
+    int updateDataSource(@Param("dataSource") DataSource dataSource);
+
 
 }
