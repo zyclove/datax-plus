4bb5d457b60ddc713b522dbddffe599196f2f3ae
LiuLi <nox60@hotmail.com>
2021-12-29 18:50:00 +0800

    Update DbColumn.java

--- datax.plus/src/main/java/com/datax/plus/model/view/DbColumn.java
+++ datax.plus/src/main/java/com/datax/plus/model/view/DbColumn.java
@@ -3,6 +3,15 @@ package com.datax.plus.model.view;
 public class DbColumn {
     private String columnName;
     private String columnType;
+    private String targetTemp = "";
+
+    public String getTargetTemp() {
+        return targetTemp;
+    }
+
+    public void setTargetTemp(String targetTemp) {
+        this.targetTemp = targetTemp;
+    }
 
     public String getColumnName() {
         return columnName;
